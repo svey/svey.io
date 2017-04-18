5d2125ac165abd481f8e9d907d9a0256bd14410a
svey <marcus.svehlak@gmail.com>
2017-04-17 23:14:38 -0700

    'rebuild'

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', function (req, res) {
 
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('svey.io is live!');
 });
