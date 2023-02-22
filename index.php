7bde849533b8570da064b3b525b098f59c5ee989
svey <marcus.svehlak@gmail.com>
2023-02-21 19:48:42 -0800

    add routing; about; 404

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php include_once("index.html"); ?>
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+
+    case '':
+    case '/':
+        require __DIR__ . '/views/index.html';
+        break;
+
+    case '/about':
+        require __DIR__ . '/views/about.html';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.html';
+        break;
+}
+
+?>
