2cefced38383614f95d054899b9e098d46659f4e
Svey <marcus.svehlak@gmail.com>
2024-01-16 22:49:53 -0800

    grey theme update

--- app/connect.js
+++ app/connect.js
@@ -157,7 +157,7 @@
       ctx.beginPath();
       ctx.moveTo(p.x, p.y);
       ctx.lineTo(p.closest[i].x, p.closest[i].y);
-      ctx.strokeStyle = `rgba(0, 0, 0, ${p.active})`;
+      ctx.strokeStyle = `rgba(30, 144, 255, ${p.active})`;
       ctx.stroke();
     }
   }
@@ -175,7 +175,7 @@
       if (!_this.active) return;
       ctx.beginPath();
       ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = `rgba(0, 0, 0, ${_this.active})`;
+      ctx.fillStyle = `rgba(30, 144, 255, ${_this.active})`;
       ctx.fill();
     };
   }
